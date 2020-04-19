11b37b195f1fd9fc6bceb3321153d6fffa2b2432
Beth Jackson <Mbeth.ashton@gmail.com>
2020-04-19 12:54:07 -0400

    Try Catch

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,12 +1,15 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  @invalid_option {:error, "Invalid option"}
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
-    |>Enum.each(fn {option, index} ->
+    |> Enum.each(fn {option, index} ->
       Shell.info("#{index} - #{option}")
     end)
+
     options
   end
 
@@ -16,7 +19,37 @@ defmodule DungeonCrawl.CLI.BaseCommands do
   end
 
   def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
+    case Integer.parse(answer) do
+      :error ->
+        throw @invalid_option
+      {option, _} ->
+        option - 1
+    end
+  end
+
+  def find_option_by_index(index, options) do
+    Enum.at(options, index) || throw @invalid_option
+  end
+
+  def ask_for_option(options) do
+    try do
+      options
+        |> display_options
+        |> generate_question
+        |> Shell.prompt
+        |> parse_answer
+        |> find_option_by_index(options)
+    catch
+      {:error, message} ->
+        display_error(message)
+        ask_for_option(options)
+    end
+  end
+
+  def display_error(message) do
+    Shell.cmd("clear")
+    Shell.error(message)
+    Shell.prompt("Press Enter to continue")
+    Shell.cmd("clear")
   end
 end
